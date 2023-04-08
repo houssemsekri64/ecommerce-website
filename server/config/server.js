33d6a301afecf556fe5bf1ad2a086760ffa5b6b6
Houssem <sekrihoussem.work@gmail.com>
2023-04-08 06:21:59 +0100

    fixing the server

--- server/config/server.js
+++ server/config/server.js
@@ -1,10 +1,14 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      "daapdgG+rD55vUybcuPfw==",
+      "pCPjzPT3yU2qDldstxHw9g==",
+      "R4lFWBQYiNQLlXoOkCD+9A==",
+    ],
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
